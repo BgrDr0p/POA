187f4a2d8cdd279a6ea314a631f695bf1f8c47f2
ElHassanBaghrar <32356688+ElHassanBaghrar@users.noreply.github.com>
2018-11-25 01:57:35 +0100

    add builder

---
+++ Design Pattern/Builder/src/client/Client.java
@@ -0,0 +1,7 @@
+package client;
+import kebab.*;
+public class Client {
+    public static void main(String[] args) {
+        Kebab MonKebab = new Kebab.KebabBuilder(2,2).sauce(6).oignon(5).build();
+    }
+}
